af8e3eaf7986b993c2e25f7de6fbc82205a28849
Csaba Valyi <csaba_valyi@epam.com>
2013-11-22 10:04:01 +0100

    [refactor] narrowed down the TorpedoClient

--- src/main/java/com/clean/communication/TorpedoClient.java
+++ src/main/java/com/clean/communication/TorpedoClient.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import com.clean.communication.message.MessageHandler;
 import com.clean.interfaces.GameStrategy;
 import com.clean.shipgame.GameWithShips;
 
@@ -14,6 +15,7 @@ public class TorpedoClient {
 
     private final int portNumber;
     private final String hostName;
+    private MessageHandler messageHandler;
     
     public TorpedoClient(String string) {
         hostName = string.split(":")[0];
@@ -27,34 +29,11 @@ public class TorpedoClient {
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
           ) {
         	gameWithShips.initialise();
-            String inpuLine;
-            String outputLine;
             TorpedoProtocol torpedoProtocol = new TorpedoProtocol(gameWithShips);
             out.println("greeting "+boardSize);
             out.println(gameStrategy.firstTarget());
-            while ((inpuLine = in.readLine()) != null) {
-                if (inpuLine.toLowerCase().contains("win")){
-                	System.out.format("Peter=%s %n", gameStrategy.getPeter());
-                    System.out.format("Win!%n");
-                    break;
-                }
-            	if(inpuLine.toLowerCase().contains("fire") || inpuLine.toLowerCase().contains("hit") ||
-            	   inpuLine.toLowerCase().contains("miss") || inpuLine.toLowerCase().contains("sunk")){
-            		outputLine = torpedoProtocol.processInput(inpuLine);
-	                if(inpuLine.toLowerCase().contains("fire") && !outputLine.contains("init")){
-	                	out.println(outputLine);
-			            outputLine = gameStrategy.nextTarget(inpuLine);
-			            System.out.format("fromUser=%s %n",	outputLine);
-			            out.println(outputLine);
-	                }
-	                if(outputLine.toLowerCase().equals("win")){
-			            out.println(outputLine);
-			            System.out.format("Defeat!%n");
-	                    break;
-	                }
-
-            	}
-            }
+            messageHandler = new MessageHandler(out, in, gameStrategy, torpedoProtocol);
+            messageHandler.run();
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
